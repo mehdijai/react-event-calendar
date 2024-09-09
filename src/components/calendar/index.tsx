4560b6641de7eb982e90b740d1752d5fa1e7a3fd
mehdijai <mjai@squarefeet.cloud>
2024-09-09 21:18:33 +0100

    Fix date picker style

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -16,23 +16,23 @@ function Calendar({
       showOutsideDays={showOutsideDays}
       components={{
         // @ts-ignore
-        IconLeft: ({ ...props }) => <ChevronLeft color="currentColor" />,
+        IconLeft: ({ ...props }) => <ChevronLeft width={18} height={18} />,
         // @ts-ignore
-        IconRight: ({ ...props }) => <ChevronRight color="currentColor" />,
+        IconRight: ({ ...props }) => <ChevronRight width={18} height={18} />,
       }}
       modifiersStyles={{
         selected: {
-          backgroundColor: "white",
           color: "hsl(var(--foreground))",
-          borderRadius: "0.275rem",
+          borderRadius: "0.2rem",
         },
       }}
       style={{
+        color: "white",
         backgroundColor: "#1a1d21",
         border: "1px solid #363a3d",
         width: "fit-content",
         padding: "1rem",
-        borderRadius: "0.3rem"
+        borderRadius: "0.375rem",
       }}
       {...props}
     />
--- src/components/date-picker/index.tsx
+++ src/components/date-picker/index.tsx
@@ -1,8 +1,10 @@
 import { endOfWeek, startOfWeek } from "date-fns";
 import moment from "moment";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Calendar from "../calendar";
 import "./style.scss";
+import { CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 function dateInRange(date: Date, range: DateRange) {
   return moment(date).isBetween(range.from, range.to, undefined, "()");
@@ -19,6 +21,20 @@ export default function DatePicker({
   value,
   onSelected,
 }: DatePickerProps) {
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  const toggleCalendar = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+      setShowCalendar(!showCalendar);
+    },
+    [showCalendar]
+  );
+
+  useEffect(() => {
+    document.addEventListener("click", () => setShowCalendar(false));
+  }, []);
+
   const [selectedWeek, setSelectedWeek] = useState<DateRange | undefined>({
     from: startOfWeek(new Date()),
     to: endOfWeek(new Date()),
@@ -34,7 +50,29 @@ export default function DatePicker({
   }, [value]);
   return (
     <div className="date-picker_wrapper">
-      <div className="date-picker shad-dialog">
+      <div className="date-picker-input__wrapper" onClick={toggleCalendar}>
+        <CalendarIcon width={18} height={18} />
+        <span>
+          {selectedWeek?.from ? (
+            selectedWeek.to ? (
+              <>
+                {moment(selectedWeek.from).format("DD/MM/YYYY")} -{" "}
+                {moment(selectedWeek.to).format("DD/MM/YYYY")}
+              </>
+            ) : (
+              moment(selectedWeek.from).format("DD/MM/YYYY")
+            )
+          ) : (
+            <span>Pick a date</span>
+          )}
+        </span>
+      </div>
+      <div
+        className={cn("date-picker", {
+          show: showCalendar,
+        })}
+        onClick={(e) => e.stopPropagation()}
+      >
         <Calendar
           mode="range"
           onDayClick={(day) => {
